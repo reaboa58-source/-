9bed8f5dc45f188121a69a45c10ff95ce78cd594
reaboa58-source <reaboa58@gmail.com>
2026-04-22 03:39:31 +0300

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    token: process.env.DISCORD_TOKEN,
-    clientId: process.env.CLIENT_ID,
+    token: process.env.MTQ5NDEwNTUyNjYyMjIyODU0MA.GWaWQF.mpl8zPFxMYHlaSbDlP3v4oUBhiotasfE0BKW14,
+    clientId: process.env.1494105526622228540,
     prefix: process.env.PREFIX || '!',
     intents: [
         'Guilds',
